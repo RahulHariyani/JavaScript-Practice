a4fb53a830ef566c2809efb77998899df4dc4314
RahulHariyani <127029815+RahulHariyani@users.noreply.github.com>
2023-03-05 18:38:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function rdh(){
+    let input = document.getElementById("Userinput").value;
+    if(input%2 !=0)
+    {
+        alert("Number is Odd")
+    }
+    else {
+        alert("Number is Even")
+    }
+    }
